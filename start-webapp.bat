1893ac7d9117be43370ea9cd8499acff3bd3542f
iiko38 <jake@okii.co.uk>
2025-08-22 19:11:28 +0100

    Add convenience scripts for launching webapp - Add start-webapp.bat for Windows - Add start-webapp.ps1 for PowerShell

---
+++ start-webapp.bat
@@ -0,0 +1,4 @@
+@echo off
+echo Starting 3D Model Library Webapp...
+cd webapp
+npm run dev
---
+++ start-webapp.ps1
@@ -0,0 +1,3 @@
+Write-Host "Starting 3D Model Library Webapp..." -ForegroundColor Green
+Set-Location "webapp"
+npm run dev
